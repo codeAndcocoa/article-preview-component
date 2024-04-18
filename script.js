94f8a29e59092a3eb57c9f427730caf0c50581d5
Naglaa Raslan <naglaa.raslan@gmail.com>
2024-04-19 01:40:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,19 @@ const shareIcon = document.querySelector(".share .fa-share");
 const shareBar = document.querySelector(".social-bar");
 const authorBar = document.querySelector(".author");
 
+const activeState = () => {
+  shareBar.classList.toggle("showBar");
+  if (shareBar.classList.contains("showBar")) {
+    shareIcon.style.backgroundColor = "hsl(214, 17%, 51%)";
+    shareIcon.style.color = "hsl(210, 46%, 95%)";
+  }else{
+    shareIcon.style.backgroundColor = "hsl(210, 46%, 95%)";
+    shareIcon.style.color = "hsl(214, 17%, 51%)";
+  }
+};
 //Event handlers
 document.addEventListener("DOMContentLoaded", () => {
-  shareIcon.addEventListener("click", () => {
-    shareBar.classList.toggle("showBar");
-   
-    /* shareIcon.style.backgroundColor = "hsl(217, 19%, 35%)";
-    shareIcon.style.color = "hsl(210, 46%, 95%)";
-   
-  }); */
+  shareIcon.addEventListener('click', () =>{
+    activeState();
+  })
 });
-shareIcon.addEventListener("click", () => { shareIcon.classList.toggle("changeColors");});
-});
-
-// hsl(217, 19%, 35%); /* Very Dark Grayish Blue */
-//   --color2: hsl(214, 17%, 51%); /* Desaturated Dark Blue */
-//   --color3: hsl(212, 23%, 69%); /* Grayish Blue */
-//   --color4: hsl(210, 46%, 95%);
